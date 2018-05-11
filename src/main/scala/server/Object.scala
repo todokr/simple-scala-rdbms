9a37d0247826abfff098692b217541ab574250f6
todokr <shunsuke.tadokoro@bizreach.co.jp>
2018-05-12 01:00:53 +0900

    insert/select

--- src/main/scala/server/Object.scala
+++ src/main/scala/server/Object.scala
@@ -1,5 +1,17 @@
 package server
 
-class Database
+import scala.collection.mutable.ArrayBuffer
 
-class Table
+case class Table(name: String, columns: Seq[Column]) {
+  val data: ArrayBuffer[Tuple] = ArrayBuffer()
+
+  def insert(value: Map[String, Any]): Unit =
+    data += Tuple(columns.map(c => c -> value.get(c.name)).toMap)
+
+  def select(columns: Seq[String]): Seq[Tuple] =
+    data.map(t => Tuple(columns.map(c => Column(c) -> t.row(Column(c))).toMap))
+}
+
+case class Tuple(row: Map[Column, Option[Any]])
+
+case class Column(name: String)
